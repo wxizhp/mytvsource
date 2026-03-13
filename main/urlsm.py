65733b0bdefe4d186fc51b257ea1ab5f52f0d935
WX <wx@com>
2026-03-13 15:30:24 +0800

    1

--- main/urlsm.py
+++ main/urlsm.py
@@ -15,7 +15,7 @@ urls = [
     "https://raw.githubusercontent.com/52liulian/iptv365/refs/heads/main/live_lite.txt",
     "https://raw.githubusercontent.com/bj123sd/IPTV/refs/heads/main/hycg.txt",
     "https://live.zbds.top/tv/iptv4.txt",
-    "https://live.hacks.tools/tv/ipv4/categories/%E5%A4%AE%E8%A7%86%E9%A2%91%E9%81%93.txt",
+    "https://live.hacks.tools/tv/ipv4/categories/%E5%A4%AE%E8%A7%86%E9%A2%91%E9%81%93.txt"
 ]
 
 
